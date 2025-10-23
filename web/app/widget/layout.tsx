0547afdb6539e3c2b304b23e4d6acef61608c5da
maytees <mateespublicprofile@gmail.com>
2025-10-23 18:42:51 -0400

    feat: trigger button framer motion

--- web/app/widget/layout.tsx
+++ web/app/widget/layout.tsx
@@ -11,7 +11,9 @@ export default function WidgetLayout({
 }) {
 	return (
 		<html lang="en" className={`${inter.className} max-w-fit`}>
-			<body className="bg-transparent max-w-fit">{children}</body>
+			<body className="flex flex-row items-center justify-center w-16 h-16 overflow-hidden bg-transparent max-w-16 max-h-16">
+				{children}
+			</body>
 		</html>
 	);
 }
--- web/app/widget/trigger/page.tsx
+++ web/app/widget/trigger/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { AnimatePresence, motion } from "motion/react";
 import Image from "next/image";
 import { type MouseEventHandler, useEffect, useId } from "react";
 import { Button } from "@/components/ui/button";
@@ -9,39 +10,54 @@ const TriggerButton = () => {
 	const {
 		data: session,
 		isPending, //loading state
+		error,
 	} = authClient.useSession();
 
 	const triggerId = useId();
 
 	useEffect(() => {
-		console.log(document.title, "is title");
-		console.log(session?.user.email, "is email");
-	}, [isPending]);
+		console.log(session, "session");
+		console.log(session?.user, "session.user");
+		console.log(isPending, "is pending");
+		console.log(error, "error");
+	}, [isPending, error, session]);
 
 	const handleWidgetOpen: MouseEventHandler<HTMLButtonElement> = (e) => {
+		console.log(session, "session");
+		console.log(session?.user, "session.user");
+		console.log(isPending, "is pending");
+		console.log(error, "error");
 		window.parent.postMessage({ type: "OPEN_FORM" }, "*");
 	};
 
 	return (
-		<Button
-			disabled={isPending}
-			id={triggerId}
-			className={cn(
-				"rounded-full border-2 border-border size-14",
-				!session?.user && "hidden",
+		<AnimatePresence>
+			{session?.user && (
+				<motion.div
+					initial={{ opacity: 0, scale: 0 }}
+					animate={{ opacity: 1, scale: 1 }}
+					whileTap={{ rotate: -25, scale: 1.05 }}
+					key={"reviseo-trigger"}
+				>
+					<Button
+						disabled={isPending}
+						id={triggerId}
+						className={cn("rounded-full border-2 border-border size-14")}
+						variant={"secondary"}
+						onClick={handleWidgetOpen}
+					>
+						{/* <Bug className={"text-foreground"} /> */}
+						<Image
+							priority
+							src={"/logo.svg"}
+							width={35}
+							height={35}
+							alt={"Reviseo Logo"}
+						/>
+					</Button>
+				</motion.div>
 			)}
-			variant={"secondary"}
-			onClick={handleWidgetOpen}
-		>
-			{/* <Bug className={"text-foreground"} /> */}
-			<Image
-				priority
-				src={"/logo.svg"}
-				width={35}
-				height={35}
-				alt={"Reviseo Logo"}
-			/>
-		</Button>
+		</AnimatePresence>
 	);
 };
 
