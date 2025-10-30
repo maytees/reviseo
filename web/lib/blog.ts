bd8b4e16c87e4b092e4424fb2975ff7d41382bc6
maytees <mateespublicprofile@gmail.com>
2025-10-29 22:13:05 -0400

    chore: use node import

--- web/lib/blog.ts
+++ web/lib/blog.ts
@@ -1,6 +1,6 @@
-import fs from "fs";
+import fs from "node:fs";
+import path from "node:path";
 import matter from "gray-matter";
-import path from "path";
 import { remark } from "remark";
 import html from "remark-html";
 import type { BlogItem } from "./types";
